4956ffacee2f4e23bf048bf8bd3aefc40d9523f3
Alexandre Chabrolin <alexandre.chabrolin@ledger.fr>
2021-08-18 10:46:55 +0200

    export all implemented families

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,6 +3,15 @@ export * from "./types";
 export * from "./rawTypes";
 export * from "./families/ethereum/types";
 export * from "./families/bitcoin/types";
+export * from "./families/algorand/types";
+export * from "./families/cosmos/types";
+export * from "./families/crypto_org/types";
+export * from "./families/polkadot/types";
+export * from "./families/ripple/types";
+export * from "./families/stellar/types";
+export * from "./families/tezos/types";
+export * from "./families/tron/types";
+
 export { default as FAMILIES } from "./families/types";
 
 export { default as Mock } from "./mock";
