b0fb384d5d2258092303a978f589fcd0f93efd9e
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-01-17 16:07:28 +0100

    misc: add FIXME comment to families enum

--- packages/client/src/families/types.ts
+++ packages/client/src/families/types.ts
@@ -1,4 +1,12 @@
 /**
+ * FIXME: enum name and members sould be in UperCamelCase format
+ * (i.e: Families, Bitcoin, etc...)
+ * cf. https://www.typescriptlang.org/docs/handbook/enums.html
+ * Beware, this will be a breaking change, should be done alongside another
+ * major release
+ */
+
+/**
  * Supported coin families
  */
 enum FAMILIES {
