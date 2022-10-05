aba71a53eedf513dd6723a5fb7ef759b04a120be
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-10-05 15:19:01 +0200

    ✨ add account.list method

---
+++ packages/server/src/handlers/listAccounts.test.ts
@@ -0,0 +1,167 @@
+import {
+  Account,
+  CryptoCurrency,
+  Currency,
+  CurrencyType,
+  ERC20TokenCurrency,
+  Logger,
+  TokenStandard,
+} from "@ledgerhq/wallet-api-core";
+import BigNumber from "bignumber.js";
+import listAccounts from "./listAccounts";
+
+const BITCOIN_CRYPTO: CryptoCurrency = {
+  type: CurrencyType.CryptoCurrency,
+  family: "bitcoin",
+  color: "color",
+  id: "bitcoin",
+  ticker: "BTC",
+  name: "Bitcoin",
+  units: [
+    {
+      name: "bitcoin",
+      code: "BTC",
+      magnitude: 8,
+    },
+  ],
+};
+
+const ETHEREUM_CRYPTO: CryptoCurrency = {
+  type: CurrencyType.CryptoCurrency,
+  family: "ethereum",
+  color: "color",
+  id: "ethereum",
+  ticker: "ETH",
+  name: "Ethereum",
+  units: [
+    {
+      name: "ether",
+      code: "ETH",
+      magnitude: 18,
+    },
+  ],
+};
+
+const USDC_TOKEN: ERC20TokenCurrency = {
+  type: CurrencyType.TokenCurrency,
+  parent: "ethereum",
+  color: "color",
+  id: "ethereum/erc20/usdc",
+  ticker: "USDC",
+  name: "USDC",
+  units: [
+    {
+      name: "ether",
+      code: "ETH",
+      magnitude: 18,
+    },
+  ],
+  contract: "constractAddress",
+  standard: TokenStandard.ERC20,
+};
+
+const cryptoAccounts: Account[] = [
+  {
+    id: "account-btc-1",
+    name: "Bitcoin 1",
+    address: "address",
+    currency: "bitcoin",
+    balance: new BigNumber(42),
+    spendableBalance: new BigNumber(42),
+    blockHeight: 1,
+    lastSyncDate: new Date("1995-12-17T03:24:00"),
+  },
+  {
+    id: "account-eth-1",
+    name: "Ethereum 1",
+    address: "address",
+    currency: "ethereum",
+    balance: new BigNumber(42),
+    spendableBalance: new BigNumber(42),
+    blockHeight: 1,
+    lastSyncDate: new Date("1995-12-17T03:24:00"),
+  },
+];
+
+const tokenAccounts: Account[] = [
+  {
+    id: "account-usdc-1",
+    name: "USDC 1",
+    address: "address",
+    currency: "ethereum/erc20/usdc",
+    balance: new BigNumber(42),
+    spendableBalance: new BigNumber(42),
+    blockHeight: 1,
+    lastSyncDate: new Date("1995-12-17T03:24:00"),
+  },
+];
+
+const serverAccounts: Account[] = [...cryptoAccounts, ...tokenAccounts];
+
+const cryptoCurrencies: CryptoCurrency[] = [BITCOIN_CRYPTO, ETHEREUM_CRYPTO];
+
+const tokenCurrencies: ERC20TokenCurrency[] = [USDC_TOKEN];
+
+const serverCurrencies: Currency[] = [...cryptoCurrencies, ...tokenCurrencies];
+
+describe("listAccounts", () => {
+  const params = { params: { params: { includeTokens: true } } };
+  const defaultLogger = new Logger("Wallet-API-Server");
+
+  test("should return empty array if no server params provided", () => {
+    expect(listAccounts(params, undefined)).toEqual([]);
+  });
+
+  test("should return empty array if no currencies or accounts available in server params", () => {
+    expect(listAccounts(params, { logger: defaultLogger })).toEqual([]);
+  });
+
+  test("should return server accounts if no JSON RPC params provided", () => {
+    const res = listAccounts(undefined, {
+      logger: defaultLogger,
+      accounts: serverAccounts,
+      currencies: serverCurrencies,
+    }) as Currency[];
+
+    expect(res).toEqual(serverAccounts);
+  });
+
+  test("should return server accounts if no input params provided", () => {
+    const res = listAccounts(
+      {},
+      {
+        logger: defaultLogger,
+        accounts: serverAccounts,
+        currencies: serverCurrencies,
+      }
+    ) as Currency[];
+
+    expect(res).toEqual(serverAccounts);
+  });
+
+  test("should not include tokens accounts when includeTokens not provided", () => {
+    const res = listAccounts(
+      { params: {} },
+      {
+        logger: defaultLogger,
+        accounts: serverAccounts,
+        currencies: serverCurrencies,
+      }
+    ) as Currency[];
+
+    expect(res).toEqual(cryptoAccounts);
+  });
+
+  test("should include all accounts when includeTokens provided", () => {
+    const res = listAccounts(
+      { params: { includeTokens: true } },
+      {
+        logger: defaultLogger,
+        accounts: serverAccounts,
+        currencies: serverCurrencies,
+      }
+    ) as Currency[];
+
+    expect(res).toEqual(serverAccounts);
+  });
+});
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -11,6 +11,7 @@ import {
   JSONRPCServerAndClient,
   JSONRPCServerMiddleware,
 } from "json-rpc-2.0";
+import listAccounts from "./handlers/listAccounts";
 import listCurrencies from "./handlers/listCurrencies";
 import globStringToRegex from "./utils/globStringToRegex";
 
@@ -19,6 +20,7 @@ const defaultLogger = new Logger("Wallet-API-Server");
 export type ServerParams = {
   logger: Logger;
   currencies?: Currency[];
+  accounts?: Account[];
 };
 
 export default class Server {
@@ -91,6 +93,7 @@ export default class Server {
     this.logger.log("connected", this.transport);
 
     this.serverAndClient.addMethod("currency.list", listCurrencies);
+    this.serverAndClient.addMethod("account.list", listAccounts);
   }
 
   /**
@@ -105,10 +108,9 @@ export default class Server {
   /**
    * Adds an handler method to the server.
    */
-  addMethod<M extends Exclude<keyof MethodsHandlerMap, "currency.list">>(
-    method: M,
-    handler: MethodsHandlerMap[M]
-  ): void {
+  addMethod<
+    M extends Exclude<keyof MethodsHandlerMap, "currency.list" | "account.list">
+  >(method: M, handler: MethodsHandlerMap[M]): void {
     if (!this.serverAndClient) {
       this.logger.error(`not connected - addMethod(${method})`);
       throw new Error("Client not connected");
