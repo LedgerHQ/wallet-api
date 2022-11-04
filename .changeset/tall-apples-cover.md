1fcf7bc963a77498b2073a9b31ce913517be5126
Kant <quentin.jaccarino@ledger.fr>
2022-11-04 14:16:31 +0100

    fix: export errors, types & rxjs

---
+++ .changeset/tall-apples-cover.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/wallet-api-server": patch
+---
+
+fix: export errors, types & rxjs
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,4 @@
+export * from "rxjs";
+export * from "./errors";
+export * from "./types";
 export * from "./WalletAPIServer";
