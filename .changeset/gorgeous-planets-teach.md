f9ad9a9d03b9e638897c63a29229d29f1c073f08
Quentin Jaccarino <quentin.jaccarino@ledger.fr>
2022-11-17 07:10:04 +0100

    fix: add HWTransport export (#44)

---
+++ .changeset/gorgeous-planets-teach.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/wallet-api-client": patch
+---
+
+fix: add HWTransport export
--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,6 @@
 /* istanbul ignore file */
 
+export { default as HWTransport } from "@ledgerhq/hw-transport";
 export * from "@ledgerhq/wallet-api-core";
 export * from "./TransportWalletAPI";
 export * from "./WalletAPIClient";
