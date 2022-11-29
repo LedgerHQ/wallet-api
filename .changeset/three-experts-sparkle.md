3994b9155f57a1ac3e762be0db67d06f6016cff8
Kant <quentin.jaccarino@ledger.fr>
2022-11-29 11:34:09 +0100

    fix: export the new device profile

---
+++ .changeset/three-experts-sparkle.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/wallet-api-simulator": patch
+---
+
+fix: export the new device profile
--- packages/simulator/src/profiles/index.ts
+++ packages/simulator/src/profiles/index.ts
@@ -1,5 +1,7 @@
+import { deviceProfile } from "./device";
 import { standardProfile } from "./standard";
 
 export const profiles = {
   STANDARD: standardProfile,
+  DEVICE: deviceProfile,
 };
