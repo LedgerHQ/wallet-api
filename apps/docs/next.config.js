1d1362938e8065976035ccd654e3c1c682277d68
Anthony Goussot <73408295+ComradeAERGO@users.noreply.github.com>
2023-06-14 18:30:08 +0200

    Adding more hooks (#188)

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
-})
- 
-module.exports = withNextra()
+const withNextra = require("nextra")({
+  defaultShowCopyCode: true,
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra();
