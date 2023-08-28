e490154ceefcd22798e2d2530c809f0117c5a078
Philip Zudemberg <philip.zdb@gmail.com>
2023-08-28 10:26:57 +0200

    chore: add validator attributes to cosmos transaction interface

--- packages/core/src/families/cosmos/types.ts
+++ packages/core/src/families/cosmos/types.ts
@@ -8,12 +8,19 @@ import type {
 
 export type CosmosOperationMode = z.infer<typeof schemaCosmosOperationMode>;
 
+export type CosmosDelegationInfo = {
+  address: string;
+  amount: BigNumber;
+};
+
 export interface CosmosTransaction extends TransactionCommon {
   readonly family: RawCosmosTransaction["family"];
   mode: CosmosOperationMode;
   fees?: BigNumber;
   gas?: BigNumber;
   memo?: string;
+  sourceValidator?: string;
+  validators?: CosmosDelegationInfo[];
 }
 
 export type RawCosmosTransaction = z.infer<typeof schemaRawCosmosTransaction>;
--- packages/core/src/families/cosmos/validation.ts
+++ packages/core/src/families/cosmos/validation.ts
@@ -10,10 +10,17 @@ export const schemaCosmosOperationMode = z.enum([
   "claimRewardCompound",
 ]);
 
+const cosmosDelegationInfo = z.object({
+  address: z.string(),
+  amount: z.string(),
+});
+
 export const schemaRawCosmosTransaction = schemaTransactionCommon.extend({
   family: z.literal(schemaFamilies.enum.cosmos),
   mode: schemaCosmosOperationMode,
   fees: z.string().optional(),
   gas: z.string().optional(),
   memo: z.string().optional(),
+  sourceValidator: z.string().optional(),
+  validators: z.array(cosmosDelegationInfo).optional(),
 });
