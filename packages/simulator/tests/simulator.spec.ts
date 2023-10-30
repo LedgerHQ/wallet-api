ff1869a51f9560a221b9c6fd20e7f31c955a9c71
Anthony Goussot <anthony.goussot@ledger.fr>
2023-10-30 10:18:49 +0100

    test(simulator): Improving readability

--- packages/simulator/tests/simulator.spec.ts
+++ packages/simulator/tests/simulator.spec.ts
@@ -5,15 +5,19 @@ import BigNumber from "bignumber.js";
 describe("Simulator", () => {
   describe("bitcoin.getXPub", () => {
     it("should return the xpub", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const xPub = await client.bitcoin.getXPub("accountId");
 
+      // THEN
       expect(xPub).toBe("xpub");
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -25,12 +29,14 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.bitcoin.getXPub("accountId")).rejects.toThrow(
         "permission",
       );
     });
 
     it("should throw an error if method not handled by server", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         methods: {},
@@ -39,25 +45,28 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.bitcoin.getXPub("accountId")).rejects.toThrow(
         "not implemented",
       );
     });
   });
-});
 
-describe("Simulator", () => {
   describe("account.list", () => {
     it("should return the list of accounts", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const accounts = await client.account.list();
 
+      // THEN
       expect(accounts).toBeInstanceOf(Array);
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -69,23 +78,28 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.account.list()).rejects.toThrow("permission");
     });
   });
 
   describe("account.request", () => {
     it("should return the requested account", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const account = await client.account.request({
         currencyIds: ["bitcoin"],
       });
 
+      // THEN
       expect(account).toBeDefined();
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -97,6 +111,7 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(
         client.account.request({ currencyIds: ["bitcoin"] }),
       ).rejects.toThrow("permission");
@@ -105,11 +120,14 @@ describe("Simulator", () => {
 
   describe("account.receive", () => {
     it("should return the account address", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const address = await client.account.receive("accountId");
 
+      // THEN
       expect(address).toBeDefined();
     });
 
@@ -125,6 +143,7 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.account.receive("accountId")).rejects.toThrow(
         "permission",
       );
@@ -133,11 +152,14 @@ describe("Simulator", () => {
 
   describe("currency.list", () => {
     it("should return a list of currencies", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const currencies = await client.currency.list();
 
+      // THEN
       expect(currencies).toBeInstanceOf(Array);
       currencies.forEach((currency) => {
         expect(currency).toHaveProperty("color");
@@ -157,12 +179,15 @@ describe("Simulator", () => {
     });
 
     it("should return a filtered list of currencies", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const currencyIds = ["btc", "eth"];
       const currencies = await client.currency.list({ currencyIds });
 
+      // THEN
       expect(currencies).toBeDefined();
       expect(currencies.length).toBe(currencyIds.length);
       expect(currencies[0]?.id).toBe(currencyIds[0]);
@@ -170,6 +195,7 @@ describe("Simulator", () => {
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -181,22 +207,27 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.currency.list()).rejects.toThrow("permission");
     });
   });
 
   describe("message.sign", () => {
     it("should return the signed message", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const message = Buffer.from("Hello, world!", "utf-8");
       const signedMessage = await client.message.sign("accountId", message);
 
+      // THEN
       expect(signedMessage).toBeInstanceOf(Buffer);
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -207,14 +238,16 @@ describe("Simulator", () => {
 
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
-
       const message = Buffer.from("Hello, world!", "utf-8");
+
+      // THEN
       await expect(client.message.sign("accountId", message)).rejects.toThrow(
         "permission",
       );
     });
 
     it("should throw an error if method not handled by server", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         methods: {},
@@ -222,8 +255,9 @@ describe("Simulator", () => {
 
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
-
       const message = Buffer.from("Hello, world!", "utf-8");
+
+      // THEN
       await expect(client.message.sign("accountId", message)).rejects.toThrow(
         "not implemented",
       );
@@ -232,19 +266,22 @@ describe("Simulator", () => {
 
   describe("storage.get & storage.set", () => {
     it("should set and get a value", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const key = "testKey";
       const value = "testValue";
-
       await client.storage.set(key, value);
       const retrievedValue = await client.storage.get(key);
 
+      // THEN
       expect(retrievedValue).toBe(value);
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -259,6 +296,7 @@ describe("Simulator", () => {
       const key = "testKey";
       const value = "testValue";
 
+      // THEN
       await expect(client.storage.set(key, value)).rejects.toThrow(
         "permission",
       );
@@ -266,6 +304,7 @@ describe("Simulator", () => {
     });
 
     it("should throw an error if method not handled by server", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         methods: {},
@@ -277,6 +316,7 @@ describe("Simulator", () => {
       const key = "testKey";
       const value = "testValue";
 
+      // THEN
       await expect(client.storage.set(key, value)).rejects.toThrow(
         "not implemented",
       );
@@ -285,49 +325,54 @@ describe("Simulator", () => {
   });
 
   describe("transaction.sign", () => {
+    const transaction = {
+      family: "ethereum",
+      amount: new BigNumber(1000000000000000000), // 1 ETH in wei
+      recipient: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B", // Vitalik Buterin's address
+      gasPrice: new BigNumber(20000000000), // 20 Gwei
+      gasLimit: new BigNumber(21000),
+      networkInfo: {
+        family: "ethereum",
+        feeItems: {
+          items: [
+            {
+              key: "0",
+              speed: "high",
+              gasPrice: "20000000000",
+            },
+            {
+              key: "1",
+              speed: "standard",
+              gasPrice: "20000000000",
+            },
+            {
+              key: "2",
+              speed: "low",
+              gasPrice: "20000000000",
+            },
+          ],
+          defaultFee: "20000000000",
+        },
+      },
+    } as const;
+
     it("should return the signed transaction", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
-      const transaction = {
-        family: "ethereum",
-        amount: new BigNumber(1000000000000000000), // 1 ETH in wei
-        recipient: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B", // Vitalik Buterin's address
-        gasPrice: new BigNumber(20000000000), // 20 Gwei
-        gasLimit: new BigNumber(21000),
-        networkInfo: {
-          family: "ethereum",
-          feeItems: {
-            items: [
-              {
-                key: "0",
-                speed: "high",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "1",
-                speed: "standard",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "2",
-                speed: "low",
-                gasPrice: "20000000000",
-              },
-            ],
-            defaultFee: "20000000000",
-          },
-        },
-      } as const;
+      // WHEN
       const signedTransaction = await client.transaction.sign(
         "accountId",
         transaction,
       );
 
+      // THEN
       expect(signedTransaction).toBe("signedTransaction");
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -339,42 +384,13 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
-      const transaction = {
-        family: "ethereum",
-        amount: new BigNumber(1000000000000000000), // 1 ETH in wei
-        recipient: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B", // Vitalik Buterin's address
-        gasPrice: new BigNumber(20000000000), // 20 Gwei
-        gasLimit: new BigNumber(21000),
-        networkInfo: {
-          family: "ethereum",
-          feeItems: {
-            items: [
-              {
-                key: "0",
-                speed: "high",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "1",
-                speed: "standard",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "2",
-                speed: "low",
-                gasPrice: "20000000000",
-              },
-            ],
-            defaultFee: "20000000000",
-          },
-        },
-      } as const;
       await expect(
         client.transaction.sign("accountId", transaction),
       ).rejects.toThrow("permission");
     });
 
     it("should throw an error if method not handled by server", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         methods: {},
@@ -383,36 +399,7 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
-      const transaction = {
-        family: "ethereum",
-        amount: new BigNumber(1000000000000000000), // 1 ETH in wei
-        recipient: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B", // Vitalik Buterin's address
-        gasPrice: new BigNumber(20000000000), // 20 Gwei
-        gasLimit: new BigNumber(21000),
-        networkInfo: {
-          family: "ethereum",
-          feeItems: {
-            items: [
-              {
-                key: "0",
-                speed: "high",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "1",
-                speed: "standard",
-                gasPrice: "20000000000",
-              },
-              {
-                key: "2",
-                speed: "low",
-                gasPrice: "20000000000",
-              },
-            ],
-            defaultFee: "20000000000",
-          },
-        },
-      } as const;
+      // THEN
       await expect(
         client.transaction.sign("accountId", transaction),
       ).rejects.toThrow("not implemented");
@@ -421,15 +408,18 @@ describe("Simulator", () => {
 
   describe("wallet.userId", () => {
     it("should return the userId", async () => {
+      // GIVEN
       const transport = getSimulatorTransport(profiles.STANDARD);
       const client = new WalletAPIClient(transport);
 
+      // WHEN
       const userId = await client.wallet.userId();
 
       expect(userId).toBe("userId");
     });
 
     it("should throw an error if permission not granted", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         permissions: {
@@ -441,10 +431,12 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.wallet.userId()).rejects.toThrow("permission");
     });
 
     it("should throw an error if method not handled by server", async () => {
+      // GIVEN
       const profile = {
         ...profiles.STANDARD,
         methods: {},
@@ -453,6 +445,7 @@ describe("Simulator", () => {
       const transport = getSimulatorTransport(profile);
       const client = new WalletAPIClient(transport);
 
+      // THEN
       await expect(client.wallet.userId()).rejects.toThrow("not implemented");
     });
   });
