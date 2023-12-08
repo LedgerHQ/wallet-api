dfd841157f221786660397fc94d679cfe1ec26ab
Anthony Goussot <anthony.goussot@ledger.fr>
2023-12-08 11:19:59 +0100

    feat(core): Add type support for events

--- packages/core/src/spec/rpcHandlers/AppHandlers.ts
+++ packages/core/src/spec/rpcHandlers/AppHandlers.ts
@@ -1,3 +1,5 @@
-export type AppHandlers = {
+export type UnknownCustomEvent = Record<`event.${string}`, undefined>;
+
+export type AppHandlers<GenericCustomEvent = UnknownCustomEvent> = {
   "event.account.updated": undefined;
-};
+} & GenericCustomEvent;
