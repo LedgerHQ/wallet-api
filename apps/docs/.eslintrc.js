8b05dc1276720559a3c6137f2835035858c0a0b8
Ramy EL BEHEDY <32883529+RamyEB@users.noreply.github.com>
2024-01-08 13:04:51 +0100

    Feat/merge doc dev portal (#283)
    * add token in action

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    extends: ["custom/next"],
-    parserOptions: {
-      project: ["./tsconfig.json"],
-      tsconfigRootDir: __dirname,
-      babelOptions: {
-        presets: [require.resolve("next/babel")], // https://github.com/vercel/next.js/issues/40687#issuecomment-1264177674
-      },
+  extends: ["custom/next"],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    babelOptions: {
+      presets: [require.resolve("next/babel")], // https://github.com/vercel/next.js/issues/40687#issuecomment-1264177674
     },
-    rules: {},
-  };
+  },
+  rules: {},
+};
