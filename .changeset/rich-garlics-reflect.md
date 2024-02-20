25305e8153fc0ad534be41839e5e76411731d8a2
Quentin Jaccarino <quentin.jaccarino@ledger.fr>
2024-02-20 14:43:31 +0100

    fix(simulator): remove process usage (#308)
    * fix(simulator): remove process usage
    bumping picomatch to remove process usage
    refactor device simulator profile to remove process usage
    
    * chore: add empty prettierrc for vscode format

---
+++ .changeset/rich-garlics-reflect.md
@@ -0,0 +1,9 @@
+---
+"@ledgerhq/wallet-api-simulator": patch
+"@ledgerhq/wallet-api-server": patch
+---
+
+fix(simulator): remove process usage
+
+bumping picomatch to remove process usage
+refactor device simulator profile to remove process usage
--- packages/simulator/src/profiles/device/index.ts
+++ packages/simulator/src/profiles/device/index.ts
@@ -3,69 +3,73 @@ import TransportHttp from "@ledgerhq/hw-transport-http";
 import type { SimulatorProfile } from "../../types";
 import { standardProfile } from "../standard";
 
-const deviceProxyUrl = process.env["DEVICE_PROXY_URL"];
-const httpTransport = deviceProxyUrl
-  ? TransportHttp(deviceProxyUrl.split("|"))
-  : undefined;
-let transport: HWTransport | undefined;
+export const deviceProfile: (deviceProxyUrl?: string) => SimulatorProfile = (
+  deviceProxyUrl,
+) => {
+  const httpTransport = deviceProxyUrl
+    ? TransportHttp(deviceProxyUrl.split("|"))
+    : undefined;
 
-export const deviceProfile: SimulatorProfile = {
-  ...standardProfile,
-  permissions: {
-    ...standardProfile.permissions,
-    methodIds: [
-      ...standardProfile.permissions.methodIds,
-      "device.transport",
-      "device.select",
-      "device.open",
-      "device.exchange",
-      "device.close",
-    ],
-  },
-  methods: {
-    ...standardProfile.methods,
-    "device.transport": async () => {
-      if (!httpTransport) {
-        throw new Error("Proxy not setup");
-      }
-      if (transport) {
-        throw new Error("Transport already opened");
-      }
-      transport = await httpTransport.create(3000, 5000);
-      return "1";
+  let transport: HWTransport | undefined;
+
+  return {
+    ...standardProfile,
+    permissions: {
+      ...standardProfile.permissions,
+      methodIds: [
+        ...standardProfile.permissions.methodIds,
+        "device.transport",
+        "device.select",
+        "device.open",
+        "device.exchange",
+        "device.close",
+      ],
     },
-    "device.select": () => "",
-    "device.open": async () => {
-      if (!httpTransport) {
-        throw new Error("Proxy not setup");
-      }
-      if (transport) {
-        throw new Error("Transport already opened");
-      }
-      transport = await httpTransport.create(3000, 5000);
-      return "1";
+    methods: {
+      ...standardProfile.methods,
+      "device.transport": async () => {
+        if (!httpTransport) {
+          throw new Error("Proxy not setup");
+        }
+        if (transport) {
+          throw new Error("Transport already opened");
+        }
+        transport = await httpTransport.create(3000, 5000);
+        return "1";
+      },
+      "device.select": () => "",
+      "device.open": async () => {
+        if (!httpTransport) {
+          throw new Error("Proxy not setup");
+        }
+        if (transport) {
+          throw new Error("Transport already opened");
+        }
+        transport = await httpTransport.create(3000, 5000);
+        return "1";
+      },
+      "device.exchange": async ({ apduHex, transportId }) => {
+        if (!transport) {
+          throw new Error("No open transport to close");
+        }
+        if (transportId !== "1") {
+          throw new Error("No transport open with this transportId");
+        }
+        return (await transport.exchange(Buffer.from(apduHex, "hex"))).toString(
+          "hex",
+        );
+      },
+      "device.close": async ({ transportId }) => {
+        if (!transport) {
+          throw new Error("No open transport to close");
+        }
+        if (transportId !== "1") {
+          throw new Error("No transport open with this transportId");
+        }
+        await transport.close();
+        transport = undefined;
+        return "1";
+      },
     },
-    "device.exchange": async ({ apduHex, transportId }) => {
-      if (!transport) {
-        throw new Error("No open transport to close");
-      }
-      if (transportId !== "1") {
-        throw new Error("No transport open with this transportId");
-      }
-      return (await transport.exchange(Buffer.from(apduHex, "hex"))).toString(
-        "hex",
-      );
-    },
-    "device.close": async ({ transportId }) => {
-      if (!transport) {
-        throw new Error("No open transport to close");
-      }
-      if (transportId !== "1") {
-        throw new Error("No transport open with this transportId");
-      }
-      await transport.close();
-      transport = undefined;
-      return "1";
-    },
-  },
+  };
 };
