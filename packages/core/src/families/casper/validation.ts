9fd48c21ed014bb57a5106a562d601752ebc1157
Kant <quentin.jaccarino@ledger.fr>
2024-04-30 10:00:13 +0200

    fix: casper schema family

--- packages/core/src/families/casper/validation.ts
+++ packages/core/src/families/casper/validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { schemaFamilies, schemaTransactionCommon } from "../common";
 
 export const schemaRawCasperTransaction = schemaTransactionCommon.extend({
-  family: z.literal(schemaFamilies.enum.internet_computer),
+  family: z.literal(schemaFamilies.enum.casper),
   fees: z.string(),
   transferId: z.string().optional(),
 });
