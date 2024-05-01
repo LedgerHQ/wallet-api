0b4052a5beafff31f3fa7516d2b549c05fe34e4b
chrisduma-ledger <cristian.duma-ext@ledger.fr>
2024-05-01 17:36:49 +0300

    chore: run lint and formatter

--- packages/server/src/helpers.ts
+++ packages/server/src/helpers.ts
@@ -3,20 +3,20 @@ import picomatch from "picomatch";
 
 export function matchCurrencies(
   currencies: Currency[],
-  patterns: string[]
+  patterns: string[],
 ): Currency[] {
   const matchers = patterns
     .filter((pattern) => pattern)
     .map((pattern) => picomatch(pattern));
 
   return currencies.filter((currency) =>
-    matchers.some((matcher) => matcher(currency.id))
+    matchers.some((matcher) => matcher(currency.id)),
   );
 }
 
 export function filterAccountsForCurrencies(
   accounts: Account[],
-  currencies: Currency[]
+  currencies: Currency[],
 ): Account[] {
   const currencyIds = new Set(currencies.map((currency) => currency.id));
 
