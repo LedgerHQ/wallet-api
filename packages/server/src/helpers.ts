ba9064801c713a7f5fc02c51b2f44f2f28ccf763
chrisduma-ledger <cristian.duma-ext@ledger.fr>
2024-05-01 17:29:45 +0300

    feat: optimize match currencies

--- packages/server/src/helpers.ts
+++ packages/server/src/helpers.ts
@@ -3,33 +3,20 @@ import picomatch from "picomatch";
 
 export function matchCurrencies(
   currencies: Currency[],
-  patterns: string[],
+  patterns: string[]
 ): Currency[] {
-  const matchedCurrencies: Currency[] = [];
-  const patternCount = patterns.length;
-  const currencyCount = currencies.length;
+  const matchers = patterns
+    .filter((pattern) => pattern)
+    .map((pattern) => picomatch(pattern));
 
-  for (let i = 0; i < patternCount; i += 1) {
-    const currentPattern = patterns[i];
-    if (currentPattern) {
-      const isMatch = picomatch(currentPattern);
-
-      for (let j = 0; j < currencyCount; j += 1) {
-        const currentCurrency = currencies[j];
-        if (currentCurrency) {
-          if (isMatch(currentCurrency.id)) {
-            matchedCurrencies.push(currentCurrency);
-          }
-        }
-      }
-    }
-  }
-  return matchedCurrencies;
+  return currencies.filter((currency) =>
+    matchers.some((matcher) => matcher(currency.id))
+  );
 }
 
 export function filterAccountsForCurrencies(
   accounts: Account[],
-  currencies: Currency[],
+  currencies: Currency[]
 ): Account[] {
   const currencyIds = new Set(currencies.map((currency) => currency.id));
 
