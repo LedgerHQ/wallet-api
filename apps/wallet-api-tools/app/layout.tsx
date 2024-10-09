3bf4aa6fc4c1bc022d5632c522ec5acb12e3dc4f
Kant <quentin.jaccarino@ledger.fr>
2024-10-09 16:58:34 +0200

    fix(wallet-api-tools): use the correct head

--- apps/wallet-api-tools/app/layout.tsx
+++ apps/wallet-api-tools/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { PropsWithChildren } from "react";
 import "../styles/globals.css";
 import { TopBar } from "./TopBar";
+import Head from "./head";
 
 export default function RootLayout({ children }: PropsWithChildren<object>) {
   return (
     <html className="dark">
-      <head />
-      <body className="bg-white dark:bg-zinc-900 dark:text-white h-screen flex flex-col">
+      <Head />
+      <body className="flex h-screen flex-col bg-white dark:bg-zinc-900 dark:text-white">
         <TopBar />
         <main className="flex flex-1 flex-col overflow-hidden">{children}</main>
       </body>
