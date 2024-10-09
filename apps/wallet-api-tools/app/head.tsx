bce7f93d466f116105fc522478d77913f75dc3f7
Kant <quentin.jaccarino@ledger.fr>
2024-10-09 16:52:10 +0200

    feat(wallet-api-tools): disable ios zoom on input and update button text

--- apps/wallet-api-tools/app/head.tsx
+++ apps/wallet-api-tools/app/head.tsx
@@ -2,7 +2,10 @@ export default function Head() {
   return (
     <>
       <title></title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        name="viewport"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
