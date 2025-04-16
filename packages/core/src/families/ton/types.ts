8206dac5ec44a7cc259fdebdc04248364915b68f
Quentin Jaccarino <quentin.jaccarino@ledger.fr>
2025-04-16 10:50:22 +0200

    feat(ton): expose ton payload for all possible actions [LIVE-17777] (#447)

--- packages/core/src/families/ton/types.ts
+++ packages/core/src/families/ton/types.ts
@@ -1,7 +1,24 @@
+import type { Address, Cell } from "@ton/core";
 import BigNumber from "bignumber.js";
 import type { z } from "zod";
 import type { TransactionCommon } from "../index";
-import type { schemaTonComment, schemaRawTonTransaction } from "./validation";
+import type {
+  schemaRawTonTransaction,
+  schemaTonComment,
+  schemaTonPayloadAddWhitelistRaw,
+  schemaTonPayloadChangeDnsRecordRaw,
+  schemaTonPayloadCommentRaw,
+  schemaTonPayloadFormatRaw,
+  schemaTonPayloadJettonBurnRaw,
+  schemaTonPayloadJettonTransferRaw,
+  schemaTonPayloadNftTransferRaw,
+  schemaTonPayloadSingleNominatorChangeValidatorRaw,
+  schemaTonPayloadSingleNominatorWithdrawRaw,
+  schemaTonPayloadTokenBridgePaySwapRaw,
+  schemaTonPayloadTonStakersDepositRaw,
+  schemaTonPayloadUnsafeRaw,
+  schemaTonPayloadVoteForProposalRaw,
+} from "./validation";
 
 export type TonComment = z.infer<typeof schemaTonComment>;
 
@@ -9,6 +26,169 @@ export type TonTransaction = TransactionCommon & {
   readonly family: RawTonTransaction["family"];
   fees: BigNumber;
   comment: TonComment;
+  payload?: TonPayloadFormat;
 };
 
 export type RawTonTransaction = z.infer<typeof schemaRawTonTransaction>;
+
+export type TonPayloadJettonTransfer = {
+  type: "jetton-transfer";
+  queryId: bigint | null;
+  amount: bigint;
+  destination: Address;
+  responseDestination: Address;
+  customPayload: Cell | null;
+  forwardAmount: bigint;
+  forwardPayload: Cell | null;
+  knownJetton: {
+    jettonId: number;
+    workchain: number;
+  } | null;
+};
+
+export type TonPayloadJettonTransferRaw = z.infer<
+  typeof schemaTonPayloadJettonTransferRaw
+>;
+
+export type TonPayloadNftTransfer = {
+  type: "nft-transfer";
+  queryId: bigint | null;
+  newOwner: Address;
+  responseDestination: Address;
+  customPayload: Cell | null;
+  forwardAmount: bigint;
+  forwardPayload: Cell | null;
+};
+
+export type TonPayloadNftTransferRaw = z.infer<
+  typeof schemaTonPayloadNftTransferRaw
+>;
+
+export type TonPayloadComment = {
+  type: "comment";
+  text: string;
+};
+
+export type TonPayloadCommentRaw = z.infer<typeof schemaTonPayloadCommentRaw>;
+
+export type TonPayloadUnsafe = {
+  type: "unsafe";
+  message: Cell;
+};
+
+export type TonPayloadUnsafeRaw = z.infer<typeof schemaTonPayloadUnsafeRaw>;
+
+export type TonPayloadJettonBurn = {
+  type: "jetton-burn";
+  queryId: bigint | null;
+  amount: bigint;
+  responseDestination: Address;
+  customPayload: Cell | Buffer | null;
+};
+
+export type TonPayloadJettonBurnRaw = z.infer<
+  typeof schemaTonPayloadJettonBurnRaw
+>;
+
+export type TonPayloadAddWhitelist = {
+  type: "add-whitelist";
+  queryId: bigint | null;
+  address: Address;
+};
+
+export type TonPayloadAddWhitelistRaw = z.infer<
+  typeof schemaTonPayloadAddWhitelistRaw
+>;
+
+export type TonPayloadSingleNominatorWithdraw = {
+  type: "single-nominator-withdraw";
+  queryId: bigint | null;
+  amount: bigint;
+};
+
+export type TonPayloadSingleNominatorWithdrawRaw = z.infer<
+  typeof schemaTonPayloadSingleNominatorWithdrawRaw
+>;
+
+export type TonPayloadSingleNominatorChangeValidator = {
+  type: "single-nominator-change-validator";
+  queryId: bigint | null;
+  address: Address;
+};
+
+export type TonPayloadSingleNominatorChangeValidatorRaw = z.infer<
+  typeof schemaTonPayloadSingleNominatorChangeValidatorRaw
+>;
+
+export type TonPayloadTonStakersDeposit = {
+  type: "tonstakers-deposit";
+  queryId: bigint | null;
+  appId: bigint | null;
+};
+
+export type TonPayloadTonStakersDepositRaw = z.infer<
+  typeof schemaTonPayloadTonStakersDepositRaw
+>;
+
+export type TonPayloadVoteForProposal = {
+  type: "vote-for-proposal";
+  queryId: bigint | null;
+  votingAddress: Address;
+  expirationDate: number;
+  vote: boolean;
+  needConfirmation: boolean;
+};
+
+export type TonPayloadVoteForProposalRaw = z.infer<
+  typeof schemaTonPayloadVoteForProposalRaw
+>;
+
+export type TonPayloadChangeDnsRecord = {
+  type: "change-dns-record";
+  queryId: bigint | null;
+  record:
+    | {
+        type: "wallet";
+        value: {
+          address: Address;
+          capabilities: {
+            isWallet: boolean;
+          } | null;
+        } | null;
+      }
+    | {
+        type: "unknown";
+        key: Buffer;
+        value: Cell | null;
+      };
+};
+
+export type TonPayloadChangeDnsRecordRaw = z.infer<
+  typeof schemaTonPayloadChangeDnsRecordRaw
+>;
+
+export type TonPayloadTokenBridgePaySwap = {
+  type: "token-bridge-pay-swap";
+  queryId: bigint | null;
+  swapId: Buffer;
+};
+
+export type TonPayloadTokenBridgePaySwapRaw = z.infer<
+  typeof schemaTonPayloadTokenBridgePaySwapRaw
+>;
+
+export type TonPayloadFormat =
+  | TonPayloadComment
+  | TonPayloadJettonTransfer
+  | TonPayloadNftTransfer
+  | TonPayloadUnsafe
+  | TonPayloadJettonBurn
+  | TonPayloadAddWhitelist
+  | TonPayloadSingleNominatorWithdraw
+  | TonPayloadSingleNominatorChangeValidator
+  | TonPayloadTonStakersDeposit
+  | TonPayloadVoteForProposal
+  | TonPayloadChangeDnsRecord
+  | TonPayloadTokenBridgePaySwap;
+
+export type TonPayloadFormatRaw = z.infer<typeof schemaTonPayloadFormatRaw>;
--- packages/core/src/families/ton/validation.ts
+++ packages/core/src/families/ton/validation.ts
@@ -6,8 +6,136 @@ export const schemaTonComment = z.object({
   text: z.string(),
 });
 
+export const schemaTonPayloadJettonTransferRaw = z.object({
+  type: z.literal("jetton-transfer"),
+  queryId: z.union([z.string(), z.null()]),
+  amount: z.string(),
+  destination: z.string(),
+  responseDestination: z.string(),
+  customPayload: z.union([z.string(), z.null()]),
+  forwardAmount: z.string(),
+  forwardPayload: z.union([z.string(), z.null()]),
+  knownJetton: z.union([
+    z.object({
+      jettonId: z.number(),
+      workchain: z.number(),
+    }),
+    z.null(),
+  ]),
+});
+
+export const schemaTonPayloadNftTransferRaw = z.object({
+  type: z.literal("nft-transfer"),
+  queryId: z.union([z.string(), z.null()]),
+  newOwner: z.string(),
+  responseDestination: z.string(),
+  customPayload: z.union([z.string(), z.null()]),
+  forwardAmount: z.string(),
+  forwardPayload: z.union([z.string(), z.null()]),
+});
+
+export const schemaTonPayloadCommentRaw = z.object({
+  type: z.literal("comment"),
+  text: z.string(),
+});
+
+export const schemaTonPayloadUnsafeRaw = z.object({
+  type: z.literal("unsafe"),
+  message: z.string(),
+});
+
+export const schemaTonPayloadJettonBurnRaw = z.object({
+  type: z.literal("jetton-burn"),
+  queryId: z.union([z.string(), z.null()]),
+  amount: z.string(),
+  responseDestination: z.string(),
+  customPayload: z.union([z.string(), z.null()]),
+});
+
+export const schemaTonPayloadAddWhitelistRaw = z.object({
+  type: z.literal("add-whitelist"),
+  queryId: z.union([z.string(), z.null()]),
+  address: z.string(),
+});
+
+export const schemaTonPayloadSingleNominatorWithdrawRaw = z.object({
+  type: z.literal("single-nominator-withdraw"),
+  queryId: z.union([z.string(), z.null()]),
+  amount: z.string(),
+});
+
+export const schemaTonPayloadSingleNominatorChangeValidatorRaw = z.object({
+  type: z.literal("single-nominator-change-validator"),
+  queryId: z.union([z.string(), z.null()]),
+  address: z.string(),
+});
+
+export const schemaTonPayloadTonStakersDepositRaw = z.object({
+  type: z.literal("tonstakers-deposit"),
+  queryId: z.union([z.string(), z.null()]),
+  appId: z.union([z.string(), z.null()]),
+});
+
+export const schemaTonPayloadVoteForProposalRaw = z.object({
+  type: z.literal("vote-for-proposal"),
+  queryId: z.union([z.string(), z.null()]),
+  votingAddress: z.string(),
+  expirationDate: z.number(),
+  vote: z.boolean(),
+  needConfirmation: z.boolean(),
+});
+
+export const schemaTonPayloadChangeDnsRecordRaw = z.object({
+  type: z.literal("change-dns-record"),
+  queryId: z.union([z.string(), z.null()]),
+  record: z.union([
+    z.object({
+      type: z.literal("wallet"),
+      value: z.union([
+        z.object({
+          address: z.string(),
+          capabilities: z.union([
+            z.object({
+              isWallet: z.boolean(),
+            }),
+            z.null(),
+          ]),
+        }),
+        z.null(),
+      ]),
+    }),
+    z.object({
+      type: z.literal("unknown"),
+      key: z.string(),
+      value: z.union([z.string(), z.null()]),
+    }),
+  ]),
+});
+
+export const schemaTonPayloadTokenBridgePaySwapRaw = z.object({
+  type: z.literal("token-bridge-pay-swap"),
+  queryId: z.union([z.string(), z.null()]),
+  swapId: z.string(),
+});
+
+export const schemaTonPayloadFormatRaw = z.union([
+  schemaTonPayloadCommentRaw,
+  schemaTonPayloadJettonTransferRaw,
+  schemaTonPayloadNftTransferRaw,
+  schemaTonPayloadUnsafeRaw,
+  schemaTonPayloadJettonBurnRaw,
+  schemaTonPayloadAddWhitelistRaw,
+  schemaTonPayloadSingleNominatorWithdrawRaw,
+  schemaTonPayloadSingleNominatorChangeValidatorRaw,
+  schemaTonPayloadTonStakersDepositRaw,
+  schemaTonPayloadVoteForProposalRaw,
+  schemaTonPayloadChangeDnsRecordRaw,
+  schemaTonPayloadTokenBridgePaySwapRaw,
+]);
+
 export const schemaRawTonTransaction = schemaTransactionCommon.extend({
   family: z.literal(schemaFamilies.enum.ton),
   fees: z.string(),
   comment: schemaTonComment,
+  payload: schemaTonPayloadFormatRaw.optional(),
 });
