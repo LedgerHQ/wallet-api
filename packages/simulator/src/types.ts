a83fda2d2a2710bdd95abd5cd647f3e978597f5a
Kant <quentin.jaccarino@ledger.fr>
2025-11-19 18:29:16 +0100

    feat: add debug option to SimulatorProfile and conditionally log messages

--- packages/simulator/src/types.ts
+++ packages/simulator/src/types.ts
@@ -2,6 +2,7 @@ import type { Account, Currency, Permission } from "@ledgerhq/wallet-api-core";
 import type { ServerConfig, WalletHandlers } from "@ledgerhq/wallet-api-server";
 
 export type SimulatorProfile = {
+  debug?: boolean;
   config: ServerConfig;
   // TODO: remove omit<Permission, "currencyIds"> in next major release of core
   permissions: Omit<Permission, "currencyIds">;
